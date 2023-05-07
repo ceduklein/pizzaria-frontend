33db602e294191ffe6770cd5075daf35e0a5e7d3
Carlos Klein <c.edudepaula@yahoo.com.br>
2023-05-06 23:36:03 -0300

    03 - Correlçai UseEffect - AuthContext

--- src/contexts/Autcontext.tsx
+++ src/contexts/Autcontext.tsx
@@ -62,7 +62,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         signOut();
       })
     }
-  })
+  },[]);
 
   async function signIn({ email, password}: SignInProps) {
     try {
